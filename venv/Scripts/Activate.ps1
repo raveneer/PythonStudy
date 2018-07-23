903af9f0b02fe813cac5ef594f0cc1193a908f80
joohokim <ravenerkr@gmail.com>
2018-07-23 10:38:35 +0900

    환경설정이 깨져있어 새로 수정.

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\PyProjects\PythonStudy\PythonStudy\venv"
+$env:VIRTUAL_ENV="C:\PyProjects\PythonStudy\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!C:\PyProjects\PythonStudy\PythonStudy\venv\Scripts\python.exe
+#!C:\PyProjects\PythonStudy\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==39.1.0'
 import re
