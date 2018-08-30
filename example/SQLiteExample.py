d00a207e5b4cb33d39ff8376bdde210156590ee4
joohokim <ravenerkr@gmail.com>
2018-08-30 10:51:51 +0900

    디비의 검색기능 연습. fetchAll이 잘 안되네. 하지만 타임아웃이니 여기까지 DA

--- example/SQLiteExample.py
+++ example/SQLiteExample.py
@@ -12,3 +12,21 @@ class DBController():
         self.cur.execute("create table hanbit_books(title varchar(100), author varchar(100), translator varchar(100), "
                          "pub_date date, isbn varchar(100))")
         self.con.commit()
+
+    def add_data(self):
+        self.cur.execute("insert into hanbit_books values (?, ?, ?, ?, ?)",
+                         ("책이름", "저자이름", "번역자이름", "2016-08-22", "12345678"))
+        self.con.commit()
+
+    def select_fetch_one(self):
+        # 왜 끝에 , 를 넣고, ()로 묶어주는 것인지?
+        self.cur.execute("select * from hanbit_books where author = ?", ('톨킨',))
+        print(self.cur.fetchone())
+        return (list(self.cur.fetchone()))
+
+    def select_fetch_all(self):
+        self.cur.execute("select * from hanbit_books where author = ?", ('톨킨',))
+        result = self.cur.fetchall()
+        for row in result:
+            print(row)
+        return (list(self.cur.fetchall()))
--- example/test_db.py
+++ example/test_db.py
@@ -13,3 +13,23 @@ class TestDBController():
     #     db_controller = example.SQLiteExample.DBController()
     #     db_controller.connect()
     #     db_controller.create_table()
+
+    # def test_add_data(self):
+    #     db_controller = example.SQLiteExample.DBController()
+    #     db_controller.connect()
+    #     db_controller.add_data()
+
+    def test_fetch_one(self):
+        db_controller = example.SQLiteExample.DBController()
+        db_controller.connect()
+        one_book = db_controller.select_fetch_one()
+        assert '호빗' == one_book[0]
+        assert '톨킨' == one_book[1]
+
+    def test_fetch_all(self):
+        db_controller = example.SQLiteExample.DBController()
+        db_controller.connect()
+        all_book = db_controller.select_fetch_all()
+        assert list == type(all_book)
+        assert ('반지전쟁', '톨킨', '몰라', '2017-01-01', '0000') == list[0]
+
